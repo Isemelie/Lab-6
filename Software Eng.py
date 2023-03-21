9c5f75309aeaafae918cb48ee6a66e44c6b8bc84
Isemelie <Isemeliepierre@ufl.edu>
2023-03-21 16:06:30 -0400

    changed menu funcation and encoder funcation

--- Software Eng.py
+++ Software Eng.py
@@ -1,17 +1,25 @@
 
-def password_changer(changer):
-    for x in changer:
-            changer=int(x+3)
-    return changer
+def password_encoder(password):
+    string = ""
+    for number in password:
+        new_number = int(number) + 3
+        string += str(new_number)
+    return string
 
-print(
-"""Menu 
--------------
-1. Encode  
-2. Decode  
-3. Quit """)
-options=input("Please enter an option:")
-password=input("Please enter your password to encode:")
+def password_decoder(new_password):
+    pass
 
-print(password_changer(password))
-
+if __name__ == '__main__':
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        print()
+        options = int(input("Please enter an option: "))
+        password = input("Please enter your password to encode:")
+        if options == 1:
+            new_password = password_encoder(password)
+            print("Your password has been encoded and stored!")
+        if options == 2:
+            new = password_decoder(new_password)
+            print(f"The encoded password is",{new_password},"The original password is", {new})
+        if options == 3:
+            break
