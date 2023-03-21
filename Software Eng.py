6521312e2661c1d80b26f328deba637ee6fe4f14
whatwhyhow1111 <ambar.aballoruiz@ufl.edu>
2023-03-21 16:49:20 -0400

    new decode function

--- Software Eng.py
+++ Software Eng.py
@@ -7,19 +7,25 @@ def password_encoder(password):
     return string
 
 def password_decoder(new_password):
-    pass
+    new_string = ""
+    for number in new_password:
+        old_number = int(number) - 3
+        new_string += str(old_number)
+    return new_string
 
 if __name__ == '__main__':
     while True:
         print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
         print()
         options = int(input("Please enter an option: "))
-        password = input("Please enter your password to encode:")
-        if options == 1:
-            new_password = password_encoder(password)
-            print("Your password has been encoded and stored!")
-        if options == 2:
-            new = password_decoder(new_password)
-            print(f"The encoded password is",{new_password},"The original password is", {new})
         if options == 3:
             break
+        if options == 1:
+            password = input("Please enter your password to encode: ")
+            new_password = password_encoder(password)
+            print("Your password has been encoded and stored!")
+            print()
+        if options == 2:
+            new = password_decoder(new_password)
+            print(f"The encoded password is " +new_password+ ", and the original password is " +new+".")
+            print()
